5e02fd2125dd86b485fcab65590247f4b3ddb228
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2022-10-17 18:49:01 -0300

    build: make component types on build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
@@ -18,5 +19,8 @@ export default defineConfig({
       }
     }
   },
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    dts()
+  ]
 })
