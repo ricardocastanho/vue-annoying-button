a8a5f7ee417f19b980fb4537e9341b17d12fe823
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2022-10-18 23:42:01 -0300

    feat: add a login page example

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <header>
-    <v-annoying-button :annoy="true" class="submit-button">
-      Annoy me
-    </v-annoying-button>
-  </header>
+  <form>
+    <fieldset>
+      <h1>Login</h1>
+
+      <input type="text" name="email" placeholder="Your e-mail">
+      
+      <input type="password" name="password" placeholder="Your password">
+
+      <v-annoying-button :annoy="true" @click.prevent>
+        Login
+      </v-annoying-button>
+    </fieldset>
+  </form>
 </template>
 
 <style scoped>
-header {
-  margin: auto;
-  width: 50%;
+form {
+  margin: 10vw auto;
+  font-family: 'Open Sans', sans-serif;
+  font-weight: weight;
+  font-style: normal;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-
-.submit-button {
-  margin-top: 1.75rem;
-  background-color: hsla(160, 100%, 37%, 1);
-  color: #fff;
-  letter-spacing: 0.5px;
-  padding: 0.85em 2.2em;
+fieldset {
+  width: 400px;
+  max-width: 600px;
   border: none;
-  font-size: 1.5rem;
-  font-weight: 500;
-  border-radius: 4px;
-  cursor: pointer;
+  font-weight: normal;
 }
-
+input {
+  box-sizing: border-box;
+  outline: none;
+  display: block;
+  color: #333333;
+  width: 100%;
+  padding: 7px;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 10px;
+  font-family: inherit;
+  font-size: 18px;
+  height: 45px;
+}
+textarea {
+  height: 200px;
+}
+button {
+  display: block;
+  padding: 19px 39px 18px 39px;
+  color: #ffffff;
+  background: #1abc9c;
+  font-size: 18px;
+  width: 30%;
+  min-width: 150px;
+  border: 1px solid #16a085;
+  border-width: 1px 1px 3px;
+  margin-top: 50px;
+  margin-bottom: 10px;
+  cursor: pointer;
+  outline: none;
+}
+button :hover  {
+  background: #109177;
+}
 </style>
